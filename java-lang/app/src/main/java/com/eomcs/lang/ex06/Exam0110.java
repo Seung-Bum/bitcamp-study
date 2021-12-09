7510247bf46a62c9ac45105c8c31d57bdb36a596
Seung-Bum <sb910126@gmail.com>
2021-12-09 13:00:16 +0900

    '2021.12.09'

--- java-lang/app/src/main/java/com/eomcs/lang/ex06/Exam0110.java
+++ java-lang/app/src/main/java/com/eomcs/lang/ex06/Exam0110.java
@@ -7,11 +7,11 @@ public class Exam0110 {
     int age = 17;
 
     // => if (조건) 문장;
-    if (age >= 19) System.out.println("성인이다.");
+    if (age >= 19) System.out.println("성인이다.1");
 
     // => 문장을 다른 줄에 놓는 경우가 많다.
     if (age >= 19); // if 문에 빈 문장을 넣을 수 있다.
-      System.out.println("성인이다.");
+    System.out.println("성인이다.2");
     // 이런 경우 개발자가 오해할 수 있다.
     // if (조건) 뒤에 문자의 끝을 표시하는 세미콜론을 사용하지 않도록 조심하라!
 
@@ -25,7 +25,7 @@ public class Exam0110 {
     .
     out.
     println(
-        "성인이다.")
+        "성인이다.3")
     ;
   }
 }
