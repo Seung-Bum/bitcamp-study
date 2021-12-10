d18788b7a9b105b56330b638c00dd400c38c7d55
Seung-Bum <sb910126@gmail.com>
2021-12-11 01:03:24 +0900

    '2021.12.11'

--- mylist-boot/app/src/main/java/com/eomcs/mylist/ContactController.java
+++ mylist-boot/app/src/main/java/com/eomcs/mylist/ContactController.java
@@ -6,20 +6,35 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController // 클라이언트 요청을 처리하는 역할
 public class ContactController {
 
+  // 인스턴스 변수
+  // => 모든 인스턴스 메서드가 공유한다.
+  String[] contacts = new String[5];
+  int size = 0;
+
   @GetMapping("/contact/list")
   public Object list() {
-    String[] contacts = {
-        "홍길동11,hong@test.com,010-1111-2222,비트캠프",
-        "홍길동22,hong@test.com,010-1111-2222,비트캠프",
-        "홍길동33,hong@test.com,010-1111-2222,비트캠프"
-    };
-
+    String[] records = new String[size];
+    for (int i = 0; i < size; i++) {
+      records[i] = contacts[i];
+    }
     return contacts;
   }
 
   @GetMapping("/contact/add")
   public Object add(String name, String email, String tel, String company) {
-    String contact = name + "," + email + "," + tel + "," + company;
-    return contact;
+    contacts[size++] = name + "," + email + "," + tel + "," + company;
+    return size;
   }
+
+  @GetMapping("/contact/get")
+  public Object get(String email) {
+    for (int i = 0; i < size; i++) {
+      if (email.equals(contacts[i].split(",")[1])) {
+        return contacts[i];
+      }
+    }
+    return "";
+  }
+
+
 }
