0a1bc649f67f3e832b53b9fac4a72f12dc7a6f8e
Seung-Bum <sb910126@gmail.com>
2021-12-15 19:15:10 +0900

    '2021.12.15'

--- java-lang-boot/app/src/main/java/com/eomcs/study/App.java
+++ java-lang-boot/app/src/main/java/com/eomcs/study/App.java
@@ -2,11 +2,13 @@ package com.eomcs.study;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @SpringBootApplication
+@ComponentScan(basePackages={"com.eomcs.study2", "com.eomcs.study"})
 public class App {
 
   public static void main(String[] args) {
@@ -15,8 +17,6 @@ public class App {
 
   @RequestMapping("/Hello")
   String Hello() {
-    return "Hello World!";
+    return "Hello, java-lang-boot!";
   }
 }
-
-
