ed671b1c6cec456a2e30cb2e2b8bc2380aa5dc15
Seung-Bum <sb910126@gmail.com>
2021-12-17 00:51:22 +0900

    '2021.12.17'

--- mylist-boot/app/src/main/java/com/eomcs/mylist/ContactController.java
+++ mylist-boot/app/src/main/java/com/eomcs/mylist/ContactController.java
@@ -3,9 +3,7 @@ package com.eomcs.mylist;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-// 클래스가 클라이언트 요청 처리 담당자임을 표시한다.
-// 이 표시(애노테이션)가 붙어 있어야만 스프링부트가 클래스를 인식한다.
-@RestController
+@RestController 
 public class ContactController {
 
   String[] contacts = new String[5];
@@ -13,58 +11,81 @@ public class ContactController {
 
   @RequestMapping("/contact/list")
   public Object list() {
-    String[] arr = new String[size]; // 배열에 저장된 값만 복사할 새 배열을 만든다.
-    for (int i = 0; i < size; i++) {
-      arr[i] = contacts[i];// 전체 배열에서 갑이 들어 있는 항목만 복사한다.
+    String[] arr = new String[size]; 
+    for (int i = 0; i < size; i++) { 
+      arr[i] = contacts[i]; 
     }
-    return arr; // 복사한 항목들을 담고있는 새 배열을 리턴한다.
+    return arr; 
   }
 
   @RequestMapping("/contact/add")
   public Object add(String name, String email, String tel, String company) {
-    String contact = name + "," + email + "," + tel + "," + company;
-    contacts[size++] = contact;
+    contacts[size++] = createCSV(name, email, tel, company);
     return size;
   }
 
   @RequestMapping("/contact/get")
   public Object get(String email) {
-    for (int i = 0; i < size; i++) {
-      if (contacts[i].split(",")[1].equals(email)) {
-        return contacts[i];
-      }
+    int index = indexOf(email);
+    if (index == -1) {
+      return "";
     }
-    return "";
+
+    return contacts[index];
   }
 
   @RequestMapping("/contact/update")
   public Object update(String name, String email, String tel, String company) {
-    String contact = name + "," + email + "," + tel + "," + company;
-    for (int i = 0; i < size; i++) {
-      if (contacts[i].split(",")[1].equals(email)) {
-        contacts[i] = contact;
-        return 1;
-      }
+    int index = indexOf(email);
+    if (index == -1) {
+      return 0;
     }
-    return 0;
+
+    contacts[index] = createCSV(name, email, tel, company);
+    return 1;
   }
 
   @RequestMapping("/contact/delete")
   public Object delete(String email) {
-    for (int i = 0; i < size; i++) {
-      if (contacts[i].split(",")[1].equals(email)) {
-
-        // 현재 위치의 다음 항목에서 배열 끝까지 반복하며 앞으로 값을 당겨온다.
-        for (int j = i + 1; j < size; j++) {
-          contacts[j-1] = contacts[j]; 
-        }
-        size--;
-        return 1;
-      }
+    int index = indexOf(email);
+    if (index == -1) {
+      return 0;
     }
-    return 0;
+
+    remove(index);
+    return 1;
   }
 
+  // 기능: 
+  // - 입력 받은 파라미터 값을 가지고 CSV 형식으로 문자열을 만들어 준다.
+  //
+  String createCSV(String name, String email, String tel, String company) {
+    return name + "," + email + "," + tel + "," + company;
+  }
 
+  // 기능:
+  // - 이메일로 연락처 정보를 찾는다.
+  // - 찾은 연락처의 배열 인덱스를 리턴한다.
+  //
+  int indexOf(String email) {
+    for (int i = 0; i < size; i++) {
+      if (contacts[i].split(",")[1].equals(email)) { 
+        return i;
+      }
+    }
+    return -1;
+  }
+
+  // 기능:
+  // - 배열에서 지정한 항목을 삭제한다.
+  //
+  String remove(int index) {
+    String old = contacts[index];
+    for (int i = index + 1; i < size; i++) {
+      contacts[i - 1] = contacts[i];
+    }
+    size--;
+    return old;
+  }
 }
 
