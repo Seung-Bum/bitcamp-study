8190cf7f5d44c312c55942ef66c9008ed488a67d
Seung-Bum <sb910126@gmail.com>
2021-12-27 18:06:23 +0900

    '2021.12.27'

---
+++ mylist-boot/app/src/main/java/com/eomcs/mylist/ArrayList2.java
@@ -0,0 +1,95 @@
+package com.eomcs.mylist;
+
+public class ArrayList2 {
+
+  static Todo[] list = new Todo[5];
+  static int size = 0;
+
+  // 기능:
+  // - 배열에서 지정한 항목을 삭제한다.
+  //
+  static Todo remove(int index) {
+    Todo old = list[index];
+    for (int i = index + 1; i < size; i++) {
+      list[i - 1] = list[i];
+    }
+    size--;
+    return old;
+  }
+
+  // 기능:
+  // - 배열의 크기를 늘린다.
+  // - 기존 배열의 값을 복사해온다.
+  //
+  static Todo[] grow() {
+    Todo[] arr = new Todo[newLength()];
+    copy(list, arr);
+    return arr;
+  }
+
+  // 기능:
+  // - 주어진 배열에 대해 50% 증가시킨 새 배열의 길이를 알려준다.
+  //
+  static int newLength() {
+    return list.length + (list.length >> 1);
+  }
+
+  // 기능: 
+  // - 배열을 복사한다.
+  // 
+  static void copy(Todo[] source, Todo[] target) {
+    int length = source.length;
+    if (target.length < source.length) {
+      length = target.length;
+    }
+    for (int i = 0; i < length; i++) {
+      target[i] = source[i];
+    }
+  }
+
+  // 기능:
+  // - 배열에 저장된 목록만 꺼내 새 배열에 담아 리턴한다.
+  //
+  static Todo[] toArray() {
+    Todo[] arr = new Todo[size]; 
+    for (int i = 0; i < size; i++) { 
+      arr[i] = list[i]; 
+    }
+    return arr; 
+  }
+
+  // 기능:
+  // - 배열에 항목을 추가한다.
+  // - 배열이 꽉찼으면 배열의 크기를 늘린다.
+  //
+  static void add(Todo contact) {
+    if (size == list.length) { 
+      list = grow();
+    }
+    list[size++] = contact;
+  }
+
+  // 기능:
+  // - 배열의 특정 위치에 값을 변경한다.
+  // - 리턴 값: 
+  //   - 변경하기 전에 저장되어 있던 값
+  //
+  static Todo set(int index, Todo contact) {
+    if (index < 0 || index >= size) { // 값이 저장된 위치가 무효한 인덱스라면 
+      return null;
+    }
+    Todo old = list[index];
+    list[index] = contact;
+    return old;
+  }
+}
+
+
+
+
+
+
+
+
+
+
