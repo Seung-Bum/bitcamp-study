d45255b03896195c824babce342913722b9c8a8d
Seung-Bum <sb910126@gmail.com>
2022-02-14 17:06:56 +0900

    '2022.02.14'

---
+++ java-lang/app/src/main/java/com/eomcs/oop/ex12/Exam0310x.java
@@ -0,0 +1,24 @@
+// 아규먼트에 람다(lambda) 활용
+package com.eomcs.oop.ex12;
+
+public class Exam0310x {
+
+  static interface Player {
+    void play();
+  }
+
+  static void testPlayer(Player p) {
+    p.play();
+  }
+
+  public static void main(String[] args) {
+    // 로컬 클래스
+
+    testPlayer( () -> {
+      System.out.println("실행!");
+    });
+
+  }
+}
+
+
