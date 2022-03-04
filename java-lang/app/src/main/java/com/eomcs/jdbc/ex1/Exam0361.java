9e13456b2b543a0d38629c334d45971a31306505
Seung-Bum <sb910126@gmail.com>
2022-03-04 11:19:52 +0900

    '2022.03.04'

--- java-lang/app/src/main/java/com/eomcs/jdbc/ex1/Exam0361.java
+++ java-lang/app/src/main/java/com/eomcs/jdbc/ex1/Exam0361.java
@@ -8,7 +8,7 @@ public class Exam0361 {
   public static void main(String[] args) throws Exception {
     try (
         java.sql.Connection con = DriverManager.getConnection(
-            "jdbc:mysql://localhost:3306/studydb?user=study&password=1111");
+            "jdbc:mariadb://localhost:3306/studydb?user=study&password=1111");
         java.sql.Statement stmt = con.createStatement();
 
         ) {
@@ -17,6 +17,7 @@ public class Exam0361 {
       // 부모 테이블의 데이터를 참조하는 자식 테이블의 데이터를 먼저 지워야 한다.
       //
       // => 게시글을 참조하는 첨부 파일 데이터를 먼저 지운다.
+      // 자식테이블
       int count = stmt.executeUpdate(
           "delete from x_board_file where board_id = 1");
       System.out.printf("x_board_file 테이블 : %d 개 삭제 성공!\n", count);
