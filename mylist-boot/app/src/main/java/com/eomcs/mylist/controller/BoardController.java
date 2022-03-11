22296a1139bc5bb227d518483383f3a27c2d86fb
Seung-Bum <sb910126@gmail.com>
2022-03-11 20:57:56 +0900

    '2022.03.11'

--- mylist-boot/app/src/main/java/com/eomcs/mylist/controller/BoardController.java
+++ mylist-boot/app/src/main/java/com/eomcs/mylist/controller/BoardController.java
@@ -1,6 +1,5 @@
 package com.eomcs.mylist.controller;
 
-import java.sql.Date;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -23,39 +22,29 @@ public class BoardController {
   }
 
   @RequestMapping("/board/add")
-  public Object add(Board board) throws Exception {
-    board.setCreatedDate(new Date(System.currentTimeMillis()));
-    boardDao.insert(board);
-    return boardDao.countAll();
+  public Object add(Board board) {
+    return boardDao.insert(board);
   }
 
 
   @RequestMapping("/board/get")
-  public Object get(int index) throws Exception {
-    Board board = boardDao.findByNo(index);
+  public Object get(int no) {
+    Board board = boardDao.findByNo(no);
     if (board == null) {
       return "";
     }
-    boardDao.increaseViewCount(index);
+    boardDao.increaseViewCount(no);
     return board;
   }
 
   @RequestMapping("/board/update")
-  public Object update(int index, Board board) throws Exception {
-    Board old = boardDao.findByNo(index);
-    if (old == null) {
-      return 0;
-    }
-
-    board.setViewCount(old.getViewCount());
-    board.setCreatedDate(old.getCreatedDate());
-
-    return boardDao.update(index, board);
+  public Object update(Board board) {
+    return boardDao.update(board);
   }
 
   @RequestMapping("/board/delete")
-  public Object delete(int index) throws Exception {
-    return boardDao.delete(index);
+  public Object delete(int no) {
+    return boardDao.delete(no);
   }
 }
 
--- mylist-boot/app/src/main/java/com/eomcs/mylist/dao/BoardDao.java
+++ mylist-boot/app/src/main/java/com/eomcs/mylist/dao/BoardDao.java
@@ -1,5 +1,6 @@
 package com.eomcs.mylist.dao;
 
+import java.util.List;
 import com.eomcs.mylist.domain.Board;
 
 public interface BoardDao {
@@ -10,17 +11,17 @@ public interface BoardDao {
 
   int countAll();
 
-  Object[] findAll();
+  List<Board> findAll();
 
-  void insert(Board board) throws Exception;
+  int insert(Board board);
 
   Board findByNo(int no);
 
-  int update(int no, Board board) throws Exception;
+  int update(Board board);
 
-  int delete(int no) throws Exception;
+  int delete(int no);
 
-  void increaseViewCount(int no) throws Exception;
+  int increaseViewCount(int no);
 }
 
 
