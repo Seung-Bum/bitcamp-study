bcffcd0f78d196eded965ffae7cf718e32a8de9f
Seung-Bum <sb910126@gmail.com>
2022-03-18 17:24:35 +0900

    '2022.03.18'

--- mylist-boot/app/src/main/java/com/eomcs/mylist/controller/ContactController.java
+++ mylist-boot/app/src/main/java/com/eomcs/mylist/controller/ContactController.java
@@ -1,60 +1,71 @@
 package com.eomcs.mylist.controller;
 
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import com.eomcs.mylist.dao.ContactDao;
 import com.eomcs.mylist.domain.Contact;
+import com.eomcs.mylist.domain.ContactTel;
 
 @RestController 
 public class ContactController {
 
-  // @Autowired
-  // - 필드 선언부에 이 애노테이션을 붙여서 표시해 두면, 
-  //   Spring Boot가 BoardController 객체를 만들 때 BoardDao 구현체를 찾아 자동으로 주입한다. 
-  //
-  //  @Autowired
+  @Autowired
   ContactDao contactDao;
 
   @RequestMapping("/contact/list")
   public Object list() {
-    return contactDao.findAll();
+    List<Contact> contacts = contactDao.findAll();
+    for (Contact contact : contacts) {
+      contact.setTels(contactDao.findTelByContactNo(contact.getNo()));
+    }
+    return contacts;
   }
 
   @RequestMapping("/contact/add")
-  public Object add(Contact contact) throws Exception {
+  public Object add(Contact contact, String[] tel) throws Exception {
     contactDao.insert(contact);
-    return contactDao.countAll();
+    for (int i = 0; i < tel.length; i++) {
+      String[] value = tel[i].split("_");
+      if (value[1].length() == 0) {
+        continue;
+      }
+      contactDao.insertTel(new ContactTel(contact.getNo(), Integer.parseInt(value[0]), value[1]));
+    }
+    return 1;
   }
 
-
-  // index 값으로 email 받음
   @RequestMapping("/contact/get")
-  public Object get(String email) throws Exception {
-    Contact contact = contactDao.findByNo(email);
+  public Object get(int no) {
+    Contact contact = contactDao.findByNo(no);
     if (contact == null) {
       return "";
     }
-
+    contact.setTels(contactDao.findTelByContactNo(no));
     return contact;
   }
 
   @RequestMapping("/contact/update")
-  public Object update(String email, Contact contact) throws Exception {
-    Contact old = contactDao.findByNo(email); // findByNo 안에 indexOf 있음
-    if (old == null) {
-      return 0;
+  public Object update(Contact contact, String[] tel) throws Exception {
+    int count = contactDao.update(contact);
+    if (count > 0) {
+      contactDao.deleteTelByContactNo(contact.getNo());
+      for (int i = 0; i < tel.length; i++) {
+        String[] value = tel[i].split(",");
+        if (value[1].length() == 0) {
+          continue;
+        }
+        contactDao.insertTel(new ContactTel(contact.getNo(), Integer.parseInt(value[0]), value[1]));
+      }
     }
-
-    //    기존하고 같아야할 사항이 있는지
-    //    board.setViewCount(old.getViewCount());
-    //    board.setCreatedDate(old.getCreatedDate());
-
-    return contactDao.update(email, contact);
+    return count;
   }
 
   @RequestMapping("/contact/delete")
-  public Object delete(String email) throws Exception {
-    return contactDao.delete(email);
+  public Object delete(int no) throws Exception {
+    contactDao.deleteTelByContactNo(no);
+    return contactDao.delete(no);
   }
 
 }
--- mylist-boot/app/src/main/java/com/eomcs/mylist/dao/ContactDao.java
+++ mylist-boot/app/src/main/java/com/eomcs/mylist/dao/ContactDao.java
@@ -1,24 +1,47 @@
 package com.eomcs.mylist.dao;
 
+import java.util.List;
+import org.apache.ibatis.annotations.Mapper;
 import com.eomcs.mylist.domain.Contact;
+import com.eomcs.mylist.domain.ContactTel;
 
+@Mapper
 public interface ContactDao {
 
-  // 인터페이스는 객체의 메서드 호출 규칙을 정의하는 것이기 때문에 
-  // 메서드를 작성할 때 메서드 몸체(method body)를 작성하지 말아야 한다.
-  // 메서드 바디가 없는 메서드를 "추상 메서드(abstract method)"라 부른다.
-  // 메서드 호출 규칙(이름)이 제각각이면 유지보수 힘듬
+  int countAll(); 
 
-  int countAll();
+  List<Contact> findAll();
 
-  Object[] findAll();
+  int insert(Contact contact);
 
-  void insert(Contact contact) throws Exception;
+  Contact findByNo(int no);
 
-  Contact findByNo(String email);
-  int update(String email, Contact contact) throws Exception;
+  Contact findByEmail(String email);
 
-  int delete(String email) throws Exception;
+  List<Contact> findByName(String name);
 
-  //  void increaseViewCount(int no) throws Exception;
+  int update(Contact contact);
+
+  int delete(int no);
+
+  List<ContactTel> findTelByContactNo(int contactNo);
+
+  int insertTel(ContactTel tel);
+
+  int updateTel(ContactTel tel);
+
+  int deleteTel(int telNo);
+
+  int deleteTelByContactNo(int contactNo);
 }
+
+
+
+
+
+
+
+
+
+
+
