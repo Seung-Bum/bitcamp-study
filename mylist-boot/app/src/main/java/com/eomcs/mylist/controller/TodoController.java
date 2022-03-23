b81c6e97f1fd373556566e894bbad39fbc78b005
Seung-Bum <sb910126@gmail.com>
2022-03-24 01:57:11 +0900

    2022.03.24

--- mylist-boot/app/src/main/java/com/eomcs/mylist/controller/TodoController.java
+++ mylist-boot/app/src/main/java/com/eomcs/mylist/controller/TodoController.java
@@ -3,44 +3,38 @@ package com.eomcs.mylist.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import com.eomcs.mylist.dao.TodoDao;
 import com.eomcs.mylist.domain.Todo;
+import com.eomcs.mylist.service.TodoService;
 
 @RestController 
 public class TodoController {
 
   @Autowired
-  TodoDao todoDao;
+  TodoService todoService;
 
   @RequestMapping("/todo/list")
   public Object list() {
-    return todoDao.findAll(); 
+    return todoService.list();
   }
 
   @RequestMapping("/todo/add")
   public Object add(Todo todo) throws Exception {
-    todoDao.insert(todo);
-    return todoDao.countAll();
+    return todoService.add(todo);
   }
 
   @RequestMapping("/todo/update")
-  public Object update(int index, Todo todo) throws Exception {
-    Todo old = todoDao.findByNo(index);
-    if (old == null) {
-      return 0;
-    }
-    todo.setDone(old.isDone()); // 기존의 체크 정보를 그대로 가져가야 한다.
-    return todoDao.update(index, todo);
+  public Object update(Todo todo) throws Exception {
+    return todoService.update(todo);
   }
 
   @RequestMapping("/todo/check")
-  public Object check(int index, boolean done) throws Exception {
-    return todoDao.updateDone(index, done);
+  public Object check(int no, boolean done) throws Exception {
+    return todoService.updateStatus(new Todo(no, done));
   }
 
   @RequestMapping("/todo/delete")
-  public Object delete(int index) throws Exception {
-    return todoDao.delete(index);
+  public Object delete(int no) throws Exception {
+    return todoService.delete(no);
   }
 
 }
--- mylist-boot/app/src/main/java/com/eomcs/mylist/domain/Todo.java
+++ mylist-boot/app/src/main/java/com/eomcs/mylist/domain/Todo.java
@@ -1,43 +1,27 @@
 package com.eomcs.mylist.domain;
 
 public class Todo {
+  int no;
   String title;
   boolean done;
 
-  public Todo() {
-    System.out.println("Todo() 호출됨!");
+  public Todo() {}
+
+  public Todo(int no, boolean done) {
+    this.no = no;
+    this.done = done;
   }
 
-  public Todo(String csvStr) {
-    // 예) csvStr => "제목, true"
-
-    String[] values = csvStr.split(","); // 예) ["제목", "true"]
-    this.setTitle(values[0]); // 배열에 들어 있는 각 항목을 객체의 필드에 저장한다.
-    this.setDone(Boolean.valueOf(values[1]));
-
-    System.out.println("Todo(String csvStr) 호출됨!");
+  @Override
+  public String toString() {
+    return "Todo [no=" + no + ", title=" + title + ", done=" + done + "]";
   }
-
-
-  public static Todo valueOf(String csvStr) {
-    // 예) csvStr => "제목, true"
-
-    String[] values = csvStr.split(",");
-
-    Todo todo = new Todo();
-    todo.setTitle(values[0]);
-    todo.setDone(Boolean.valueOf(values[1]));
-
-    return todo;
+  public int getNo() {
+    return no;
   }
-
-  // Board 객체에서 메모장으로 보낼때 사용
-  public String toCsvString() {
-    return String.format("%s,%s", 
-        this.getTitle(), 
-        this.isDone());
+  public void setNo(int no) {
+    this.no = no;
   }
-
   public String getTitle() {
     return title;
   }
@@ -51,10 +35,6 @@ public class Todo {
     this.done = done;
   }
 
-  //  @Override
-  //  public String toString() {
-  //    return "Todo [title=" + title + ", done=" + done + "]";
-  //  }
 
 
 }
