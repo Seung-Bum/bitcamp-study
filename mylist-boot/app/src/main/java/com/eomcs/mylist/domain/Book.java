8f68e3aa578b9576e5e2a08c9600450986f585bf
Seung-Bum <sb910126@gmail.com>
2022-03-28 23:10:08 +0900

    '2022.03.28'

--- mylist-boot/app/src/main/java/com/eomcs/mylist/domain/Book.java
+++ mylist-boot/app/src/main/java/com/eomcs/mylist/domain/Book.java
@@ -1,7 +1,9 @@
 package com.eomcs.mylist.domain;
 
 import java.sql.Date;
+import lombok.Data;
 
+@Data
 public class Book {
   int no;
   String title;
@@ -11,60 +13,5 @@ public class Book {
   Date readDate;
   int page;
   int price;
-
-  @Override
-  public String toString() {
-    return "Book [no=" + no + ", title=" + title + ", author=" + author + ", press=" + press
-        + ", page=" + page + ", price=" + price + ", readDate=" + readDate + ", feed=" + feed + "]";
-  }
-  public int getNo() {
-    return no;
-  }
-  public void setNo(int no) {
-    this.no = no;
-  }
-  public String getTitle() {
-    return title;
-  }
-  public void setTitle(String title) {
-    this.title = title;
-  }
-  public String getAuthor() {
-    return author;
-  }
-  public void setAuthor(String author) {
-    this.author = author;
-  }
-  public String getPress() {
-    return press;
-  }
-  public void setPress(String press) {
-    this.press = press;
-  }
-  public int getPage() {
-    return page;
-  }
-  public void setPage(int page) {
-    this.page = page;
-  }
-  public int getPrice() {
-    return price;
-  }
-  public void setPrice(int price) {
-    this.price = price;
-  }
-  public Date getReadDate() {
-    return readDate;
-  }
-  public void setReadDate(Date readDate) {
-    this.readDate = readDate;
-  }
-  public String getFeed() {
-    return feed;
-  }
-  public void setFeed(String feed) {
-    this.feed = feed;
-  }
-
-
+  String photo;
 }
--- mylist-boot/app/src/main/java/com/eomcs/mylist/domain/Todo.java
+++ mylist-boot/app/src/main/java/com/eomcs/mylist/domain/Todo.java
@@ -1,5 +1,8 @@
 package com.eomcs.mylist.domain;
 
+import lombok.Data;
+
+@Data
 public class Todo {
   int no;
   String title;
@@ -11,30 +14,4 @@ public class Todo {
     this.no = no;
     this.done = done;
   }
-
-  @Override
-  public String toString() {
-    return "Todo [no=" + no + ", title=" + title + ", done=" + done + "]";
-  }
-  public int getNo() {
-    return no;
-  }
-  public void setNo(int no) {
-    this.no = no;
-  }
-  public String getTitle() {
-    return title;
-  }
-  public void setTitle(String title) {
-    this.title = title;
-  }
-  public boolean isDone() {
-    return done;
-  }
-  public void setDone(boolean done) {
-    this.done = done;
-  }
-
-
-
 }
